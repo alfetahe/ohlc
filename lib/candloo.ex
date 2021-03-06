c944d82c0cbeaa4260bea2e111f84d8e22002548
anuar <anuar.ssn@gmail.com>
2021-03-07 01:48:59 +0200

    candle volume and date added

--- lib/candloo.ex
+++ lib/candloo.ex
@@ -5,26 +5,27 @@ defmodule Candloo do
 
   alias Candloo.Candle
 
-  def calc_candles([[{:price, _}, {:volume, _}, {:time, _}, {:side, _}] | _] = trades, _timeframe) do
+  def calc_candles([[{:price, _}, {:volume, _}, {:time, _}, {:side, _}] | _] = trades, timeframe) do
 
-    calc_candle(trades, [], %Candle{})
+    calc_candle(trades, [], %Candle{}, timeframe)
   end
 
 
-  defp calc_candle([active_trade | trades_tail] = _trades, candles, previous_candle) do
+  defp calc_candle([active_trade | trades_tail] = _trades, candles, previous_candle, timeframe) do
 
     candle = %Candle{
       open: String.to_float(active_trade[:price]),
       high: max(String.to_float(active_trade[:price]), previous_candle.high),
       close: String.to_float(active_trade[:price]),
       low: min(String.to_float(active_trade[:price]), previous_candle.low),
-      volume: String.to_float(active_trade[:volume]) + previous_candle.volume
+      volume: String.to_float(active_trade[:volume]) + previous_candle.volume,
+      start_timestamp: round(active_trade[:time]) |> DateTime.from_unix()
     }
 
-    calc_candle(trades_tail, [candle | candles], candle)
+    calc_candle(trades_tail, [candle | candles], candle, timeframe)
   end
 
-  defp calc_candle(trades, candles, _previous_candle) when length(trades) == 0 do
+  defp calc_candle(trades, candles, _previous_candle, _timeframe) when length(trades) == 0 do
      candles
   end
 
