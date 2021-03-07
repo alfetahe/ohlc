74665142ee7f206ade5d396cba9b9027411c00fb
anuar <anuar.ssn@gmail.com>
2021-03-07 21:17:14 +0200

    bugfixes and added trades count to the candle data

--- lib/candle.ex
+++ lib/candle.ex
@@ -1,5 +1,5 @@
 defmodule Candloo.Candle do
 
-  defstruct timeframe: nil, stime: 0, etime: 0, open: 0, high: 0, low: nil, close: 0, volume: 0, trades: 0, processed: false
+  defstruct stime: 0, etime: 0, open: 0, high: 0, low: nil, close: 0, volume: 0, trades: 0, processed: false
 
 end
--- lib/candloo.ex
+++ lib/candloo.ex
@@ -5,49 +5,86 @@ defmodule Candloo do
 
   alias Candloo.Candle
 
+  @doc "Creates OHLC candles from trades."
   def create_candles([[{:price, _}, {:volume, _}, {:time, _}, {:side, _}] | _] = trades, timeframe) do
 
-    create_candle(trades, [], %Candle{}, timeframe)
+    loop_trades(trades, [%Candle{}], timeframe)
   end
 
-  defp create_candle([active_trade | trades_tail] = _trades, candles, previous_candle, timeframe) do
+  # Loops thru trades and creates or updates candles.
+  defp loop_trades([trades_head | trades_tail], [candles_head | candles_body] = candles, timeframe) do
 
-    trade_time = round(active_trade[:time]) |> DateTime.from_unix()
-    trade_price_formatted = String.to_float(active_trade[:price])
-    trade_volume_formatted = String.to_float(active_trade[:volume])
-
-    candle = if previous_candle.processed === false or !timeframe_has_candle?(previous_candle, active_trade) do
-      %Candle{
-        open: trade_price_formatted,
-        high: trade_price_formatted,
-        low: trade_price_formatted,
-        close: trade_price_formatted,
-        volume: trade_volume_formatted,
-        stime: trade_time,
-        etime: get_etime(timeframe, trade_time),
-        processed: true
-      }
-    else
-      %{ previous_candle |
-        high: max(trade_price_formatted, previous_candle.high),
-        low: min(trade_price_formatted, previous_candle.low),
-        volume: trade_volume_formatted + previous_candle.volume,
-        processed: true
-      }
+    candles = cond do
+      # Appends new candle to the candles list without the processed candle.
+      candles_head.processed === false ->
+        candle = create_candle(trades_head, timeframe)
+        [candle | candles_body]
+      # Appends new candle to the candles list.
+      !timeframe_has_candle?(candles_head, trades_head) ->
+        candle = create_candle(trades_head, timeframe)
+        [candle | candles]
+      # Appends updated candle to the candles list.
+      true ->
+        updated_candle = update_candle(candles_head, trades_head)
+        [updated_candle, candles_body]
     end
 
-    create_candle(trades_tail, [candle | candles], candle, timeframe)
+    loop_trades(trades_tail, candles, timeframe)
   end
 
-  defp create_candle(trades, candles, _previous_candle, _timeframe) when length(trades) == 0, do: candles
+  # Returns all candles.
+  defp loop_trades(trades, candles, _timeframe) when length(trades) == 0, do: candles
 
-  defp timeframe_has_candle?(previous_candle, trade) do
+  # Creates new candle.
+  defp create_candle(trade, timeframe) do
+    trade_data_formatted = format_trade_data(trade)
 
-    trade_time = round(trade[:time]) |> DateTime.from_unix()
+    %Candle{
+      open: trade_data_formatted[:price],
+      high: trade_data_formatted[:price],
+      low: trade_data_formatted[:price],
+      close: trade_data_formatted[:price],
+      volume: trade_data_formatted[:volume],
+      trades: 1,
+      stime: trade_data_formatted[:time],
+      etime: get_etime(timeframe, trade_data_formatted[:time]),
+      processed: true
+    }
+  end
 
-    previous_candle_etime = round(previous_candle[:etime]) |> DateTime.from_unix()
+  # Returns updated candle.
+  defp update_candle(candle, trade) do
+    trade_data_formatted = format_trade_data(trade)
 
-    case DateTime.compare(previous_candle_etime, trade_time) do
+    %{ candle |
+      close: trade_data_formatted[:price],
+      high: max(trade_data_formatted[:price], candle.high),
+      low: min(trade_data_formatted[:price], candle.low),
+      volume: trade_data_formatted[:volume] + candle.volume,
+      trades: 1 + candle.trades,
+      processed: true
+    }
+  end
+
+  # Formats the trade data.
+  defp format_trade_data(trade) do
+    trade = Keyword.put(trade, :time, String.to_float(trade[:time]) |> round())
+    trade = Keyword.put(trade, :price, String.to_float(trade[:price]))
+    trade = Keyword.put(trade, :volume, String.to_float(trade[:volume]))
+
+    trade
+  end
+
+  # Returns true if trade time is bigger then last candles etime.
+  defp timeframe_has_candle?(last_candle, trade) do
+
+    trade_data_formatted = format_trade_data(trade)
+
+    {:ok, trade_time_struct} = trade_data_formatted[:time] |> DateTime.from_unix()
+
+    {:ok, last_candle_etime} = last_candle.etime |> DateTime.from_unix()
+
+    case DateTime.compare(last_candle_etime, trade_time_struct) do
       :gt -> true
       :lt -> false
       :eq -> false
@@ -55,33 +92,37 @@ defmodule Candloo do
 
   end
 
+  # Formats and returns etime for candles.
   defp get_etime(timeframe, trade_time) do
 
+    {:ok, trade_time_struct} = trade_time |> DateTime.from_unix()
+
     candle_etime = %DateTime{
-      year: trade_time.year,
-      month: trade_time.month,
+      year: trade_time_struct.year,
+      month: trade_time_struct.month,
       day: 0,
       hour: 0,
       minute: 0,
       second: 0,
-      time_zone: trade_time.time_zone,
-      zone_abbr: trade_time.zone_abbr,
-      utc_offset: trade_time.utc_offset,
-      std_offset: trade_time.std_offset
+      time_zone: trade_time_struct.time_zone,
+      zone_abbr: trade_time_struct.zone_abbr,
+      utc_offset: trade_time_struct.utc_offset,
+      std_offset: trade_time_struct.std_offset
     }
 
-    case timeframe do
+    etime = case timeframe do
       :minute ->
-        end_minutes = DateTime.add(trade_time, 60, :second)
-        %{candle_etime | day: trade_time.day, hour: trade_time.hour, minute: end_minutes.minute, second: 0}
+        end_minutes = DateTime.add(trade_time_struct, 60, :second)
+        %{candle_etime | day: trade_time_struct.day, hour: trade_time_struct.hour, minute: end_minutes.minute, second: 0}
       :hour ->
-        end_hour = DateTime.add(trade_time, 60, :minute)
-        %{candle_etime | day: trade_time.day, hour: end_hour.hour, minute: 0, second: 0}
+        end_hour = DateTime.add(trade_time_struct, 60, :minute)
+        %{candle_etime | day: trade_time_struct.day, hour: end_hour.hour, minute: 0, second: 0}
       :day ->
-        end_day = DateTime.add(trade_time, 24, :hour)
+        end_day = DateTime.add(trade_time_struct, 24, :hour)
         %{candle_etime | day: end_day.day, hour: 0, minute: 0, second: 0}
     end
 
+    DateTime.to_unix(etime)
   end
 
 end
